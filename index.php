774702de4088022bc1f263bb78fdc5fe129eeae9
unknown <lynx120403@gmail.com>
2024-07-09 12:21:09 +0700

    firts commit

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <style>
+        body {
+            font-family: 'Courier New', Courier, monospace, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #FFFAFA;
+        }
+        header {
+            background: #ffc0cb;
+            color: #ffffff;
+            padding: 5px 0;
+            text-align: center;
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+            overflow: hidden;
+        }
+        #main {
+            padding: 30px;
+            background: #ffffff;
+            margin-top: 20px;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <h1 style="color: black;">WISUDA TAHUN INI</h1>
+        </div>
+    </header>
+    <div class="container">
+        <div id="main">
+            <h2>
+               <center>Persiapan Setelah Sidang</center> 
+            </h2>
+            <p>
+            SETELAH SIDANG: <br>
+            1. revisi penguji <br>
+            2. setelah acc penguji, minta TTD di lembar pengesahan <br>
+            3. ACC PENGUJI. lalu turnitin (D3 50%) link pengajuannya https://s.id/turnitinPolije  <br>
+            4. yg di up turnitin = HALAMAN COVER, RINGKASAN, BAB 1-5 <br>
+            5. upload ruang baca, jangan lupa scan lembar pengesahan, materai 10k di surat pernyataan publikasi <br>
+            6. bandel laporan <br>
+            7. buat video upload di yt (min 3), dan up kode program di git <br>
+            8. link gform pengisian link yt dan git <br>
+            9. edit surat penyerahan laporan (ada di grup angkatan) <br>
+            10. setelah ngisi link, konfirmasi di mba Riska RPL (paraf). parafnya di surat penyerahan laporan <br>
+            11. setelah paraf, minta ttd pak Beni <br>
+            12. ttd bu Endang di surat penyerahan laporan setelah validasi ruang baca <br>
+            13. serahkan laporan bandel ke perpus, minta ttd penyerahan laporan <br>
+            14. upload sipora : ringkasan, bab 1, laporan lengkap (cover-lampiran), daftar pustaka <br>
+            15. setelah dapet email acc sipora, isi form bebas tanggungan perpustakaan. https://s.id/BebasPerpustakaan <br>
+            16. scan lembar penyerahan laporan MAGANG dan TA + up sertif turnitin. kirim. <br>
+            17. selesai. <br>
+            </p>
+        </div>
+    </div>
+</body>
+</html>
